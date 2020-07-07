e378f38c96e258235d69b624144f19246fbf74e8
Dmytro Skumin <dmytro.skumin@28stone.com>
2020-07-07 18:11:44 +0300

    updates

---
+++ sitemap-routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+	<Route path='/' />
+	<Route path='/warum-bei-uns' />
+    <Route path='/was-bieten-wir' />
+	<Route path='/was-kostet-die' />
+	<Route path='/kontakt' />
+	<Route path='/impressum' />
+	<Route path='/datenschutz' />
+    </Route>
+);
